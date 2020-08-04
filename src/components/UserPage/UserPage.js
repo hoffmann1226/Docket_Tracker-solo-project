533e854b92b90b282b1c3a961502d326536fb653
Tom Hoffmann <hoffmann1226@gmail.com>
2020-08-04 14:17:40 -0500

    Get saga created

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from "../LogOutButton/LogOutButton";
 
+
 class UserPage extends Component {
+
+  componentDidMount () {
+    this.props.dispatch({type: 'FETCH_DOCKET'})
+  }
 
   // this component doesn't do much to start, just renders some user info to the DOM
   render() {
     return (
       <div>
-        <h1 id="welcome">Welcome, {this.props.user.username}!</h1>
+        <h3 id="welcome">Welcome, {this.props.user.username}!</h3>
         <p>Your ID is: {this.props.user.id}</p>
+
+
         <LogOutButton className="log-in" />
       </div>
     );
---
+++ src/redux/sagas/getSaga.js
@@ -0,0 +1,21 @@
+
+import axios from 'axios';
+import { takeEvery, put } from 'redux-saga/effects';
+
+
+function* fetchDocket(){
+    console.log('in getDocket!')
+    try{
+      const response = yield axios.get('/api/shelf');
+      console.log('got from server:', response.data)
+      yield put({type:'SET_DOCKET', payload: response.data});
+    }catch (error){
+      console.log('problem getting docket from server', error);
+    }
+  }
+
+  function* getSaga() {
+    yield takeEvery('FETCH_DOCKET', fetchDocket );
+  }
+
+export default getSaga;
