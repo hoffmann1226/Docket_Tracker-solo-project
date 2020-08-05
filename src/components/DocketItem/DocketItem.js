ed1210a3195ffc7a230a890c31ae07dac28b078c
Tom Hoffmann <hoffmann1226@gmail.com>
2020-08-05 13:35:25 -0500

    delete button click works

--- src/components/DocketItem/DocketItem.js
+++ src/components/DocketItem/DocketItem.js
@@ -5,6 +5,11 @@ import { connect } from 'react-redux';
 
 class DocketItem extends Component {
 
+handleDelete = (id)=>(event)=>{
+console.log('delete clicked, id?', id);
+this.props.dispatch({type: 'DELETE_EVENT', payload: id});
+}//end handleDelete
+
   render() {
     return (
       <tr key={this.props.item.id}>
@@ -13,7 +18,7 @@ class DocketItem extends Component {
         <td>{this.props.item.due_date}</td>
         <td><button>Notes</button></td>
         <td><button>Edit</button></td>
-        <td><button>Delete</button></td>
+        <td><button onClick = {this.handleDelete(this.props.item.id)}>Delete</button></td>
       </tr>
     );
   }
