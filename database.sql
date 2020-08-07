3bb90c16a6d6434991be5ae2e7e2f04a6ca85128
Tom Hoffmann <hoffmann1226@gmail.com>
2020-08-07 11:33:07 -0500

    database updated

--- database.sql
+++ database.sql
@@ -16,10 +16,12 @@ CREATE TABLE "event_info" (
 
 
 INSERT INTO "event_info" ("case", "event", "due_date", "details", "user_id")
-VALUES ('Hoffmann v. Prime', 'Hearing for Motion to Dismiss', '2020-08-20', 'Difficult judge', 1),
-('Johnson v. Prime', 'Oral Argument', '2020-08-22', 'In courtroom 101B', 1),
+VALUES ('Hoffmann v. Prime', 'Hearing for Motion to Dismiss', '2020-08-20', 'Courtroom C103 at 1pm.  No colorable argument on behalf of defendant, should easily survive.', 1),
+('Johnson v. Prime', 'Statute of limitations', '2020-10-23', 'Advise client so information is gathered in time to file case.  Likely large award at stake.', 1),
 ('Joachim v. Prime', 'Discovery Responses Due', '2020-08-22', 'Follow up with client on interrogatory 3', 1),
 ('Stiff v. Prime', 'Motions in Limine Due', '2020-08-25', 'Consult with client', 1),
-('Simonelli v. Prime', 'Motions in Limine Due', '2020-08-25', 'Consult with client', 2);
+('Wilcoxon v. Prime', 'Trial Ready Date', '2020-08-23', 'Firm trial date, judge is sick of granting continuances, threatened opposing counsel with sanctions if they try to move the date again.', 1),
+('Reyes v. Prime', 'Discovery Responses Due', '2020-09-29', 'Follow up with client about earnings history.  Need to corroborate allegations made in complaint.', 1),
+('Simonelli v. Prime', 'Motions in Limine Due', '2020-08-25', 'Consult with client, issue with hearsay regarding purported acceptance of offer.', 1);
 
 SELECT * FROM "event_info";
