9c7f88458369c4c0bf2fe0db433d7b37c3fda1b9
Tom Hoffmann <hoffmann1226@gmail.com>
2020-08-10 10:40:41 -0500

    search on user page works, next saga

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -3,19 +3,39 @@ import { connect } from 'react-redux';
 import DocketList from "../DocketList/DocketList";
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-
+import Input from '@material-ui/core/Input';
 
 
 class UserPage extends Component {
 
+  state = {
+    case: ''
+}
 
   addEvent =() => this.props.history.push('/add')
+
+  searchCase =() => {
+    this.props.dispatch({ type: 'SEARCH_CASE', payload: this.state })
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+            case: event.target.value
+    })
+    console.log(this.state)
+  }
+
+
 
   render() {
     return (
     <div>
       <DocketList/>
       <Button color="default" variant="contained" onClick={this.addEvent}>Add Event <AddIcon/></Button>
+      <div>
+        <p>Search by case name <Input onChange= {this.handleChange} placeholder="case name"></Input></p>
+        <Button color="default" variant="contained" onClick={this.searchCase}>Search</Button>
+      </div>
     </div>
       );
   }
