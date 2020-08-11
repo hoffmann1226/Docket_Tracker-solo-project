31ee553131bd34fefc5f25bf3f43e284016a72ae
Tom Hoffmann <hoffmann1226@gmail.com>
2020-08-11 17:19:00 -0500

    minor touch up

--- server/routes/docket.router.js
+++ server/routes/docket.router.js
@@ -38,7 +38,7 @@ router.delete('/:id', rejectUnauthenticated, (req, res) => {
       });
   });
 
-  //PUT Route for edit page  //FIX THIS TO HAVE ID  AND FIX REC.BODY.ID to REC.PARAMS.ID
+  //PUT Route for edit page
   router.put('/:id', rejectUnauthenticated, (req, res) => {
     let queryText = `UPDATE "event_info" SET "case" = $1, "event" = $2, "due_date" = $3, "details" = $4 WHERE "id" = $5;`;
     pool.query(queryText, [req.body.case, req.body.event, req.body.due_date, req.body.details, req.params.id])
